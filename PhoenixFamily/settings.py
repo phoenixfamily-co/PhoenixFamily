93529b4b7a248daef4a252363bd2374275f76915
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-11-23 20:27:22 +0330

    req added

--- PhoenixFamily/settings.py
+++ PhoenixFamily/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = '/home/abbaslot/phoenixfamily.co/'
+STATIC_ROOT = '/home/your_user_name/phoenixfamily.co/static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
