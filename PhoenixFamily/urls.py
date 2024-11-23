db98862efe3e976feb2e54eaeb89209f54742ebc
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-11-23 20:56:50 +0330

    req added

--- PhoenixFamily/urls.py
+++ PhoenixFamily/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('Home.urls')),  # مسیر URLهای اپلیکیشن home
+)
