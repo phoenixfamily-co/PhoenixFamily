e882a8b076177c7782a9810df77642c7b45b5529
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-11-26 23:02:30 +0330

    urls added

--- About/views.py
+++ About/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import get_language
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(60 * 15)
 def about(request):
     current_language = get_language()
     return render(request, 'about.html', {'LANGUAGE_CODE': current_language})
--- Contact/views.py
+++ Contact/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import get_language
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(60 * 15)
 def contact(request):
     current_language = get_language()
     return render(request, 'contact.html', {'LANGUAGE_CODE': current_language})
