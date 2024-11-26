18f3fc49b5b17669d405077c8a34991c7ea223f1
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-11-26 22:42:42 +0330

    urls added

--- PhoenixFamily/urls.py
+++ PhoenixFamily/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns(
-    path('', include('About.urls', namespace='about')),  # مسیر URLهای اپلیکیشن About
-    path('', include('Blog.urls', namespace='blogs')),  # مسیر URLهای اپلیکیشن Blog
-    path('', include('Contact.urls', namespace='contact')),  # مسیر URLهای اپلیکیشن Contact
-    path('', include('Home.urls'), namespace='home'),  # مسیر URLهای اپلیکیشن Home
-    path('', include('Product.urls', namespace='products')),  # مسیر URLهای اپلیکیشن Product
-    path('', include('User.urls', namespace='user')),  # مسیر URLهای اپلیکیشن User
+    path('about/', include('About.urls', namespace='about')),  # مسیر URLهای اپلیکیشن About
+    path('blogs/', include('Blog.urls', namespace='blogs')),  # مسیر URLهای اپلیکیشن Blog
+    path('contact/', include('Contact.urls', namespace='contact')),  # مسیر URLهای اپلیکیشن Contact
+    path('home/', include('Home.urls'), namespace='home'),  # مسیر URLهای اپلیکیشن Home
+    path('products/', include('Product.urls', namespace='products')),  # مسیر URLهای اپلیکیشن Product
+    path('user/', include('User.urls', namespace='user')),  # مسیر URLهای اپلیکیشن User
 )
