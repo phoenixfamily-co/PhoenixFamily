234cf3377c849ba45825841bc1e22864d85cab82
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-03 17:39:06 +0330

    base html added

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.utils.translation import get_language, get_language_bidi
-from django.views.decorators.cache import cache_page
 
 
-@cache_page(60 * 15)
 def home(request):
     current_language = get_language()
     is_bidi = get_language_bidi()
