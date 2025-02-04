5ee95a72580490e2ff42480a932b8b9ea5f354f9
mohammadmma <mohammadmma3004@gmail.com>
2025-02-04 19:42:27 +0330

    develop content model in Home.models
    for now just has three field which title is required only !

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    video = models.FileField(upload_to='videos_uploaded', null=True)
