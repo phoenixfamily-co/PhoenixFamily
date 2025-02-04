b8cbe96f54eba74ca82ca668597aa9ae3c279121
mohammadmma <mohammadmma3004@gmail.com>
2025-02-04 19:43:53 +0330

    develop CRUD API for Content model
    - initial serializer for Content.
    - develop view with ModelViewSets
    - configure url routing using REST DefaultRouter

--- Home/urls.py
+++ Home/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'content', views.ContentView)
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home-view'),  # صفحه اصلی
+    path('api/', include(router.urls)),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import get_language, get_language_bidi
+from .serializers import ContentSerializer
+from .models import Content
+from rest_framework import viewsets
+
 
 
 def home(request):
@@ -7,3 +11,10 @@ def home(request):
     is_bidi = get_language_bidi()
 
     return render(request, 'home.html', {'LANGUAGE_CODE': current_language, 'LANGUAGE_BIDI': is_bidi})
+# _____________________________ Class Based Views for developing API ________________________________
+
+class ContentView(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+
+
