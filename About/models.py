7a16424f2c37e0260f58b8e032973d7a038a14e2
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-09 20:44:13 +0330

    style changed

--- About/models.py
+++ About/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -13,7 +14,13 @@ class AboutUs(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
-    phone = models.CharField(max_length=13, null=True, blank=True)
+    # شماره تلفن (اعتبارسنجی شماره تلفن)
+    phone_regex = RegexValidator(
+        regex=r'^\+?(\d[\d().\s-]*)?\d{9,15}$',
+        message="Phone number must be entered in a valid international format. Examples: '+1234567890', "
+                "'+1 (234) 567-8901', or '+44-20-1234-5678'."
+    )
+    number = models.CharField(unique=True, validators=[phone_regex], max_length=15, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     linkedin = models.URLField(verbose_name="linkedin", blank=True, null=True)
     instagram = models.URLField(verbose_name="instagram", blank=True, null=True)
