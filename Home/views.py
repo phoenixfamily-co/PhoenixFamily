9e866207a49373751d3335ebc337ec45fc69187e
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-09 19:42:12 +0330

    style changed

--- Home/views.py
+++ Home/views.py
@@ -8,8 +8,18 @@ from rest_framework import viewsets
 def home(request):
     current_language = get_language()
     is_bidi = get_language_bidi()
+    content = Content.objects.all()
+    vision = Vision.objects.all()
+    features = Features.objects.all()
 
-    return render(request, 'home.html', {'LANGUAGE_CODE': current_language, 'LANGUAGE_BIDI': is_bidi})
+    return render(request, 'home.html',
+                  {'LANGUAGE_CODE': current_language,
+                   'LANGUAGE_BIDI': is_bidi,
+                   'Content': content,
+                   'Vision': vision,
+                   'Features': features
+                   },
+                  )
 
 
 # _____________________________ Class Based Views for developing API ________________________________
