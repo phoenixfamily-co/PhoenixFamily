b5855492e1b2f80b68778f63f6be880fb28418bc
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-09 20:23:16 +0330

    style changed

--- About/views.py
+++ About/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.utils.translation import get_language
+from django.utils.translation import get_language, get_language_bidi
 from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 from .models import AboutUs, FAQ
@@ -9,7 +9,16 @@ from .serializers import AboutUsSerializer, FAQSerializer
 @cache_page(60 * 15)
 def about(request):
     current_language = get_language()
-    return render(request, 'about.html', {'LANGUAGE_CODE': current_language})
+    is_bidi = get_language_bidi()
+
+    aboutUs = AboutUs.objects.first()
+    faq = FAQ.objects.all()
+
+    return render(request, 'about.html', {'LANGUAGE_CODE': current_language,
+                                          'LANGUAGE_BIDI': is_bidi,
+                                          'AboutUs': aboutUs,
+                                          'FAQ': faq
+                                          })
 
 # _____________________________ Class Based Views for developing API ________________________________
 
