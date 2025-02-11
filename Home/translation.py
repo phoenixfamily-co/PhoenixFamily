c8ce35f1c0068b517ca2a01efd5e6221fbdcf93e
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-12 01:43:37 +0330

    multi language added

--- Home/translation.py
+++ Home/translation.py
@@ -1,9 +1,9 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Content, Vision, Features
+from .models import Content  # مدل را ایمپورت کنید
 
 
 class ContentTranslationOptions(TranslationOptions):
     fields = ('title', 'description')  # فیلدهایی که باید ترجمه شوند
 
 
-translator.register(Content, ContentTranslationOptions)
+translator.register(Content, ContentTranslationOptions)  # ثبت مدل برای ترجمه
