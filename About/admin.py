87a36d649652e631c9c0705870c455467f9fac75
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-12 02:00:38 +0330

    multi language added

--- About/admin.py
+++ About/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from translations import *
+from modeltranslation.admin import TranslationAdmin
+from .models import *
 
-# Register your models here.
+
+class AboutUsAdmin(TranslationAdmin):
+    list_display = ('title', 'description', 'name', 'alt', 'context')
+
+
+admin.site.register(AboutUs, AboutUsAdmin)
+
+
+class FAQAdmin(TranslationAdmin):
+    list_display = ('title', 'description')
+
+
+admin.site.register(FAQ, FAQAdmin)
