0f0c3fae9db7b4431aa54221212c409e2294896e
mohammadmma <mohammadmma3004@gmail.com>
2025-02-12 15:27:13 +0330

    register models in admin page

--- About/admin.py
+++ About/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FAQ, AboutUs
 
 # Register your models here.
+
+admin.site.register(FAQ)
+admin.site.register(AboutUs)
--- PhoenixFamily/settings.py
+++ PhoenixFamily/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'Contact',
     'Product',
     'User',
+    'Authentication',
 
 ]
 
