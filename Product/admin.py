6412ac7ebf8f975d748d279629ce7a91563acc74
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-14 19:20:01 +0330

    multi language added

--- Product/admin.py
+++ Product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from .models import *
 
-# Register your models here.
+
+class ProductAdmin(TranslationAdmin):
+    list_display = ('title', 'description', 'name', 'header', 'content')
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class FeaturesAdmin(TranslationAdmin):
+    list_display = ('title', 'description')
+
+
+admin.site.register(Features, FeaturesAdmin)
