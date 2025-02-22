c14b4d3e42362df6c9d25305a1c99251cb9e768a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-22 19:50:06 +0330

    multi language added

--- Seo/admin.py
+++ Seo/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import SEOPage
+from modeltranslation.admin import TranslationAdmin
+
+from .models import SEOPage, Keyword
 
 
 @admin.register(SEOPage)
-class SEOPageAdmin(admin.ModelAdmin):
+class SEOPageAdmin(TranslationAdmin):
     list_display = ('page_url', 'title', 'description')
     search_fields = ('page_url', 'title')
+
+
+class KeywordAdmin(TranslationAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Keyword, KeywordAdmin)
