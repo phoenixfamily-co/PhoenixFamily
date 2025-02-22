8c21c87bd2a7ae65d225e185ff78607710f37b99
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-22 23:02:51 +0330

    multi language added

--- Seo/context_processors.py
+++ Seo/context_processors.py
@@ -1,21 +1,22 @@
 from PhoenixFamily import settings
 from .models import SEOPage
 from django.utils.translation import gettext_lazy as _
+from django.utils.encoding import force_str
 
 
 def seo_context(request):
-    path = request.path.strip("/")  # گرفتن آدرس صفحه بدون دامنه
+    path = request.path.strip("/")
     seo_data = SEOPage.objects.prefetch_related('keywords').filter(page_url=path).first()
 
     if not seo_data:
         return {
-            'seo_title': _(settings.SEO['default']['title']),
-            'seo_description': _(settings.SEO['default']['description']),
-            'seo_keywords': ", ".join([_(kw) for kw in settings.SEO['default']['keywords']]),  # تبدیل لیست به رشته
+            'seo_title': force_str(_(settings.SEO['default']['title'])),
+            'seo_description': force_str(_(settings.SEO['default']['description'])),
+            'seo_keywords': ", ".join([force_str(_(kw)) for kw in settings.SEO['default']['keywords']]),
         }
 
     return {
         'seo_title': seo_data.title,
         'seo_description': seo_data.description,
-        'seo_keywords': ", ".join([kw.name for kw in seo_data.keywords.all()]),  # دریافت کلمات کلیدی از مدل مرتبط
+        'seo_keywords': ", ".join([kw.name for kw in seo_data.keywords.all()]),
     }
