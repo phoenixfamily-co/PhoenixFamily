7296f13315ad6d2d52771800bccee920f264759c
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-02-25 20:48:58 +0330

    multi language added

--- Seo/views.py
+++ Seo/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from django.http import HttpResponse
 from Seo.models import SEOPage, Keyword
 from Seo.serializers import SEOPageSerializer, KeyWordSerializer
 
@@ -13,3 +13,6 @@ class KeywordView(viewsets.ModelViewSet):
     queryset = Keyword.objects.all()
     serializer_class = KeyWordSerializer
 
+
+def test_path(request):
+    return HttpResponse(f"Request Path: {request.path}")
